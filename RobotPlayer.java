06b3bffd66c3a4679e18a5065cb1debe9aa771e7
SrsBusiness <dchenviolinist@gmail.com>
2014-01-16 05:01:26 +0000

    Sanity came to town

--- RobotPlayer.java
+++ RobotPlayer.java
@@ -125,72 +125,16 @@ public class RobotPlayer {
         rand = new Random();
         lifeTurn = 0;
         RobotType type = rc.getType();
+        int id = 
         while (true) {
             myLocation = rc.getLocation();
             enemyDir = myLocation.directionTo(enemyHQLocation);
             switch (type) {
                 case HQ:
-                    try {
-                        broadcastIn = rc.readBroadcast(0);
-                        if (broadcastIn != 0) {
-                            robotIDs.add(broadcastIn);
-                        }
-                        if (robotIDs.size() == 5) {
-                            rc.broadcast(1, robotIDs[robotIDs.size()-1]);
-                        }
-                        //Check if a robot is spawnable and spawn one if it is
-                        if (rc.isActive() &&
-                                rc.senseRobotCount() < GameConstants.MAX_ROBOTS) {
-                            if (rc.senseObjectAtLocation(rc.getLocation().add(enemyDir)) == null) {
-                                rc.spawn(enemyDir);
-                                //Increment global robot count.
-                                //rc.setTeamMemory(0, rc.getTeamMemory()[0]+1)
-                            }
-                        }
-                        //Attack nearby enemies (range^2 = 15).
-                        Robot[] nearbyEnemies = rc.senseNearbyGameObjects(Robot.class, 15, rc.getTeam().opponent());
-                        if(nearbyEnemies.length > 0) {
-                            rc.attackSquare(rc.senseRobotInfo(getBestTarget(nearbyEnemies, rc)).location);
-                        }
-                    } catch (Exception e) {
-                        System.err.println(e.toString() + "HQ Exception");
-                    }
+                    HQ.HQ_run(); 
                     break;
                 case SOLDIER:
-                    try {
-                        if (rc.isActive()) {
-                            lifeTurn++;
-                            if (lifeTurn == 1) {
-                                rc.broadcast(0, rc.getRobot().getID());
-                            }
-                            Robot[] nearbyEnemies = rc.senseNearbyGameObjects(Robot.class, 10, rc.getTeam().opponent());
-                            if (nearbyEnemies.length > 0) {
-                                rc.attackSquare(rc.senseRobotInfo(getBestTarget(nearbyEnemies, rc)).location);
-                            } else if (
-                                       rc.senseNearbyGameObjects(Robot.class, 10, rc.getTeam()).length == 0) {
-                                rc.construct(RobotType.PASTR);
-                            } else {// if (action < 80) {
-                                Direction moveDirection = chooseDir(rc);
-                                if (rc.canMove(moveDirection)) {
-                                    rc.move(moveDirection);
-                                } else {
-                                    //Try a random direction
-                                    //TODO: try each dir
-                                    Direction randChoice = directions[rand.nextInt(8)];
-                                    if (rc.canMove(randChoice)) {
-                                        rc.move(randChoice);
-                                    }
-                                }
-                            }/* else {
-                                //Sneak towards the enemy
-                                //if (rc.canMove(toEnemy)) {
-                                //    rc.sneak(toEnemy);
-                                //}
-                            }*/
-                        }
-                    } catch (Exception e) {
-                        System.err.println(e.toString() + "Soldier Exception");
-                    }
+                    Soldier.Solder_run(); 
                     break;
                 case NOISETOWER:case PASTR:
                     break;
